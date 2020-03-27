40f143455eac976cb50b825e5f5e67fc490eeebf
Shankar Vasudevan <shankar.bala96@gmail.com>
2020-03-27 19:48:38 +0530

    fix: made some changes for the PR code

--- src/Details.js
+++ src/Details.js
@@ -1,39 +1,32 @@
 import React from "react";
-import ReactDOM from 'react-dom'
-import Form from './Form';
+import Form from "./Form";
 
 class Details extends React.Component {
-
-  constructor(props)
-  {
-    super(props)
-    this.state = { savedData: [], isFormActive: false }
+  constructor(props) {
+    super(props);
+    this.state = { savedData: [], isFormActive: false };
   }
 
-  componentDidMount()
-  {
-    setInterval(()=>{
+  componentDidMount() {
+    setInterval(() => {
       this.setState({
         savedData: this.props.savedData
       });
-      clearInterval()
-    }, 500)
+      clearInterval();
+    }, 500);
   }
 
-  getForm()
-  {
-    const { isFormActive } = this.state
-    this.setState({ isFormActive: !isFormActive })
-  }
+  getForm = () => {
+    const { isFormActive } = this.state;
+    this.setState({ isFormActive: !isFormActive });
+  };
 
   render() {
     const { isFormActive } = this.state;
     return (
       <div className="container">
         <nav className="navbar navbar-light">
-          <a className="navbar-brand" style={{ float: "left" }}>
-          Here's your bookmarks
-          </a>
+          <h5>Here's your bookmark</h5>
           <input
             type="submit"
             value="Add New"
@@ -42,25 +35,42 @@ class Details extends React.Component {
             onClick={this.getForm}
           />
         </nav>
-        {isFormActive ? <Form addData={this.props.addData}/> : null}
+        {isFormActive ? <Form addData={this.props.addData} /> : null}
         <br></br>
         <div className="row">
-          {
-        this.state.savedData.map(individualValue =>{
-        return (
-        <div className="card col-sm-3" style={{width: "18rem"}} key={individualValue.name}>
-          <a href={individualValue.url} >
-            <img src={individualValue.image} className="card-img-top" alt="logo" />
-          </a>
-          <div className="card-body">
-          <h5 className="card-title">{individualValue.title}</h5>
-            <p className="card-text">{individualValue.description}</p>
-          </div>
-          <footer><button className="btn btn-danger" id={individualValue.id} onClick={this.props.handleDelete.bind(this, individualValue)} >Delete</button></footer>
-        </div>
-        )
-        })
-        }
+          {this.state.savedData.map(individualValue => {
+            return (
+              <div
+                className="card col-sm-3"
+                style={{ width: "18rem" }}
+                key={individualValue.name}
+              >
+                <a href={individualValue.url}>
+                  <img
+                    src={individualValue.image}
+                    className="card-img-top"
+                    alt="logo"
+                  />
+                </a>
+                <div className="card-body">
+                  <h5 className="card-title">{individualValue.title}</h5>
+                  <p className="card-text">{individualValue.description}</p>
+                </div>
+                <footer>
+                  <button
+                    className="btn btn-danger"
+                    id={individualValue.id}
+                    onClick={this.props.handleDelete.bind(
+                      this,
+                      individualValue
+                    )}
+                  >
+                    Delete
+                  </button>
+                </footer>
+              </div>
+            );
+          })}
         </div>
       </div>
     );
